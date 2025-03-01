dbb665be9368d424dc4e4eade2acae3215a5103d
jimmy-eos <jimmy@kjdesign.studio>
2025-02-28 21:18:51 -0800

    (wip): KEONI

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,72 +1,83 @@
-import React, { useState, useLayoutEffect, useRef } from 'react'
-import styles from './style.module.css';
-import Image from 'next/image';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React, { useState, useLayoutEffect, useRef } from "react";
+import styles from "./style.module.css";
+import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const projects = [
-    {
-        title: "Salar de Atacama",
-        src: "salar_de_atacama.jpg"
-    },
-    {
-        title: "Valle de la luna",
-        src: "valle_de_la_muerte.jpeg"
-    },
-    {
-        title: "Miscanti Lake",
-        src: "miscani_lake.jpeg"
-    },
-    {
-        title: "Miniques Lagoons",
-        src: "miniques_lagoon.jpg"
-    },
-]
+  {
+    title: "SONG 1",
+    src: "keoni-tall.jpg",
+  },
+  {
+    title: "SONG 2",
+    src: "keoni-tall-2.jpg",
+  },
+  {
+    title: "SONG 3",
+    src: "keoni-tall-3.jpg",
+  },
+  {
+    title: "SONG 4",
+    src: "keoni-tall-4.jpg",
+  },
+];
 
 export default function Index() {
+  const [selectedProject, setSelectedProject] = useState(0);
+  const container = useRef(null);
+  const imageContainer = useRef(null);
 
-    const [selectedProject, setSelectedProject] = useState(0);
-    const container = useRef(null);
-    const imageContainer = useRef(null);
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.create({
+      trigger: imageContainer.current,
+      pin: true,
+      start: "top-=100px",
+      end: document.body.offsetHeight - window.innerHeight - 50,
+    });
+  }, []);
 
-    useLayoutEffect( () => {
-        gsap.registerPlugin(ScrollTrigger);
-        ScrollTrigger.create({
-            trigger: imageContainer.current,
-            pin: true,
-            start: "top-=100px",
-            end: document.body.offsetHeight - window.innerHeight - 50,
-        })
-    }, [])
-
-    return (
-        <div ref={container} className={styles.projects}>
-            <div className={styles.projectDescription}>
-                <div ref={imageContainer} className={styles.imageContainer}>
-                    <Image 
-                        src={`/images/${projects[selectedProject].src}`}
-                        fill={true}
-                        alt="project image"
-                        priority={true}
-                    />
-                </div>
-                <div className={styles.column}>
-                    <p>The flora is characterized by the presence of high elevation wetland, as well as yellow straw, broom sedge, tola de agua and tola amaia.</p>
-                </div>
-                <div className={styles.column}>
-                    <p>Some, like the southern viscacha, vicuña and Darwins rhea, are classified as endangered species. Others, such as Andean goose, horned coot, Andean gull, puna tinamou and the three flamingo species inhabiting in Chile (Andean flamingo, Chilean flamingo, and Jamess flamingo) are considered vulnerable.</p>
-                </div>
-            </div>
-
-            <div className={styles.projectList}>
-                {
-                    projects.map( (project, index) => {
-                        return <div key={index} onMouseOver={() => {setSelectedProject(index)}} className={styles.projectEl}>
-                            <h2>{project.title}</h2>
-                        </div>
-                    })
-                }
-            </div>
+  return (
+    <div ref={container} className={styles.projects}>
+      <div className={styles.projectDescription}>
+        <div ref={imageContainer} className={styles.imageContainer}>
+          <Image
+            src={`/images/${projects[selectedProject].src}`}
+            fill={true}
+            alt="project image"
+            priority={true}
+          />
         </div>
-    )
+        <div className={styles.column}>
+          <p>
+            Keoni is a 20 year old Filipino artist based out of Orange County,
+            California.
+          </p>
+        </div>
+        <div className={styles.column}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            malesuada, sapien sit amet tincidunt lacinia, elit quam commodo.
+          </p>
+        </div>
+      </div>
+
+      <div className={styles.projectList}>
+        {projects.map((project, index) => {
+          return (
+            <div
+              key={index}
+              onMouseOver={() => {
+                setSelectedProject(index);
+              }}
+              className={styles.projectEl}
+            >
+              <h2>{project.title}</h2>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
